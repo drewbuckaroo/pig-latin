94a3ae5f91c01cedf9cff90f4564fb321a4ccbcc
drewbuckaroo <drewmlockhart@gmail.com>
2020-08-19 11:44:56 -0700

    add function and loop in js.
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("#pigLatin").submit(function(event) {
+        event.preventDefault();
+        let word = $('input#sentence').val();
+    });
+       
+});
+function pigLatin(str) {
+    str = str.toLowerCase()
+    const vowels = ["a", "e", "i", "o", "u"];
+    let vowelIndex = 0;
+
+    if (vowels.includes(str[0])) {
+        return str + "way";
+    } else {
+        for (let char of str) {
+            if (vowels.includes(char)) {
+                vowelIndex = str.indexOf(char);
+                break;
+            }
+        }
+    }
+    return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+}
